1385916de9f86992d6a41497b3116257ec3088f2
Guoyuyoung <guoyuyoung0318@gmail.com>
2023-12-25 23:55:42 +0800

    ArbitrationControllerTest

---
+++ aftersale/src/test/java/cn/edu/xmu/oomall/aftersale/controller/ArbitrationControllerTest.java
@@ -0,0 +1,58 @@
+package cn.edu.xmu.oomall.aftersale.controller;
+
+import cn.edu.xmu.javaee.core.model.ReturnNo;
+import cn.edu.xmu.javaee.core.mapper.RedisUtil;
+import cn.edu.xmu.javaee.core.util.JwtHelper;
+import cn.edu.xmu.oomall.aftersale.AftersaleApplication;
+import cn.edu.xmu.oomall.aftersale.dao.ArbitrationDao;
+import cn.edu.xmu.oomall.aftersale.dao.bo.Aftersale;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+
+@SpringBootTest(classes = AftersaleApplication.class)
+@AutoConfigureMockMvc
+@Transactional(propagation = Propagation.REQUIRED)
+public class ArbitrationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private RedisUtil redisUtil;
+    private static String customerToken;
+    private static String visitorToken;
+    private static final String CREATE_AFTERSALE = "/aftersales/{aid}/arbitrations";
+    @BeforeAll
+    public static void setup(){
+        JwtHelper jwtHelper = new JwtHelper();
+        customerToken = jwtHelper.createToken(1L,"customer1",1L, 1, 3600);
+        visitorToken=jwtHelper.createToken(2L,"visitor1",-1L,1,3600);
+    }
+    @Test
+    void createArbitrationWhenUserSucceed() throws Exception {
+        String body = "{\"name\":\"test\", \"reason\": \"test_reason\"}";
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post(CREATE_AFTERSALE, 1L)
+                        .header("authorization", customerToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(body))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errno").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.customer_id", is(1L)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.reason", is("test_reason")))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
