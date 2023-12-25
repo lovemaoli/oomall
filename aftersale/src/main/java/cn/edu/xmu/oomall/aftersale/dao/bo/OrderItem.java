67a819359ce66ff9b66c3fd04cbf9400a86a21c0
hu'yu'xuan <1692676945@qq.com>
2023-12-26 02:13:44 +0800

    OrderItem po and bo

---
+++ aftersale/src/main/java/cn/edu/xmu/oomall/aftersale/dao/bo/OrderItem.java
@@ -0,0 +1,57 @@
+package cn.edu.xmu.oomall.aftersale.dao.bo;
+
+import cn.edu.xmu.javaee.core.aop.CopyFrom;
+import cn.edu.xmu.javaee.core.model.ReturnNo;
+import cn.edu.xmu.javaee.core.util.CloneFactory;
+//import cn.edu.xmu.oomall.aftersale.controller.dto.OrderItemDto;
+//import cn.edu.xmu.oomall.aftersale.controller.vo.OrderItemVo;
+//import cn.edu.xmu.oomall.aftersale.dao.AftersaleDao;
+import cn.edu.xmu.oomall.aftersale.dao.OrderItemDao;
+import cn.edu.xmu.oomall.aftersale.mapper.po.OrderItemPo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true, doNotUseGetters = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@CopyFrom({OrderItemPo.class})
+public class OrderItem implements Serializable {
+    private Long id;
+    private Long latest_aftersale_id;
+
+    private OrderItemDao orderItemDao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getLatest_aftersale_id() {
+        return latest_aftersale_id;
+    }
+
+    public void setLatest_aftersale_id(Long latest_aftersale_id) {
+        this.latest_aftersale_id = latest_aftersale_id;
+    }
+
+    public OrderItemDao getOrderItemDao() {
+        return orderItemDao;
+    }
+
+    public void setOrderItemDao(OrderItemDao orderItemDao) {
+        this.orderItemDao = orderItemDao;
+    }
+}
